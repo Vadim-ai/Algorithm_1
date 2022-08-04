5778203a5a5bb03ff9372f19359446a3f1eb4b83
SkyPro <vadim.phobia@gmail.com>
2022-08-04 23:21:23 +0300

    Еще не все сделал, второй коммит

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringListImpl stringList = new StringListImpl();
+        stringList.add(1,"лысый");
     }
 }
